4e033c84d250a0b61d3fc6e60b12b318b803eefa
MykhalV <mykhal0809@gmail.com>
2022-08-21 14:16:37 +0300

    Firts commit (adding objct)

--- js/scriptOne.js
+++ js/scriptOne.js
@@ -1,6 +1,22 @@
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt("How many movies you watched?", "");
 
-number = 10;
-console.log(number);
 
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+
+let a = prompt("Name of the last movie that you saw", ""),
+    b = +prompt("Make a rating from 1 to 10", ""),
+    c = prompt("Name of the last movie that you saw", ""),
+    d = +prompt("Make a rating from 1 to 10", ""); 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
