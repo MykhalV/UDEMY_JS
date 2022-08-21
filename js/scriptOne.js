9e43a184a5b4a645e210c9d354cb599979b49e5b
MykhalV <mykhal0809@gmail.com>
2022-08-21 21:15:53 +0300

    commit zadaachi tsykl

--- js/scriptOne.js
+++ js/scriptOne.js
@@ -62,16 +62,16 @@ switch(num){
 }   
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-let result = '';
-const lengthh = 7;
+// let result = '';
+// const lengthh = 7;
 
-for (let i = 1; i < lengthh; i++){
-    for(let j = 0; j < i; j++){
-        result += '*';
-    }
-    result += '\n';
-}
-console.log(result);
+// for (let i = 1; i < lengthh; i++){
+//     for(let j = 0; j < i; j++){
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+// console.log(result);
 
 /////////////////////////////////////
 
@@ -117,3 +117,50 @@ for(let i = 5; i <= 10; i++){
     console.log(numb);
 }
 
+////////////////////////////////////////////////////////////////////////////
+
+// 1
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for(let i = 0; i < arr.length; i++){
+//    result[i] = arr[i];
+// }
+
+
+
+// 2
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for(let i = 0; i < data.length; i++){
+//     if(typeof(data[i]) == 'number'){
+//         data[i] = data[i]* 2;
+//     }else if(typeof(data[i]) == 'string'){
+//         data[i] = `${data[i]} - done`;
+//     }
+// }
+
+
+//3
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+
+// for (let i = 1; i <= data.length; i++) {
+//     result[i - 1] = data[data.length - i];
+// }
+// console.log(result);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+// console.log(result);
