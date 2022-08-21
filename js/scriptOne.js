e641536d7b91a9b518d134a130d42e92cab85061
MykhalV <mykhal0809@gmail.com>
2022-08-21 16:04:04 +0300

    commit 2

--- js/scriptOne.js
+++ js/scriptOne.js
@@ -23,4 +23,40 @@ personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
-// Test commit 1
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+if(4 == 9){
+    console.log("ok!");
+}else{
+    console.log("Error");
+}
+
+////////////////////////////
+
+// if(num < 49){
+//     console.log("Error");
+// }else if(num>100){
+//     console.log("A lot");
+// }else {
+//     console.log("Ok!");
+// }
+// /////////////////////////////
+
+// (num === 50) ? console.log("ok!") : console.log("Error!");
+// /////////////////////////////
+const num = 50;
+switch(num){
+    case 49:
+        console.log("Wrong");
+        break;
+    case 100:
+        console.log("Wrong");
+        break;
+    case 50:
+        console.log("True");
+        break;
+    default:
+        console.log("vse ploho");
+        break;
+}   
